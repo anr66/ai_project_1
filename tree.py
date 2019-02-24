6c8682bdf1e1393ce23f17155c0e9ce71fa67bb0
Maria <anr66@zips.uakron.edu>
2019-02-24 01:21:03 -0500

    Add progress so far

--- tree.py
+++ tree.py
@@ -1,6 +1,8 @@
 
 import sys
 import csv
+import pickle
+import pandas as pd
 
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
@@ -10,27 +12,46 @@ from sklearn.metrics import confusion_matrix
 
 
 def case_1():
+    print("hello")
 
-	file_str = input("Enter the name of the data file\n")
-	
-	with open(file_str) as csv_file:
-		csv_reader = csv.reader(csv_file, delimeter=',')
-		line_count = 0
-		for row in csv_reader:
-			if (line_count == 0):
-				print("Header {", ".join(row)}")
-				line_count += 1
-			else:
-				classifier = DecisionTreeClassifier(min_samples_split = 100)
-				features = list(data2)
-				train, test = train_test_split(data2, test_size = .1)
-				x_train = train[features]
-				X_test = test[features]
-				decisiontree = classifier.fit(x_train, y_train)
-				
-	
+    file_str = input("Enter the name of the data file\n")
+
+    data = pd.read_csv(file_str, sep=',')
+
+    print("Length: ", len(data))
+    #print("Shape: ", data.shape())
+
+    #print("Data: ", data.head())
+
+    X = data.values[:, 0:1]
+    Y = data.values[:, 0]
+
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3,
+ random_state = 100)
+
+    clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100, max_depth = 3, min_samples_leaf = 100)
+
+    tree = clf_gini.fit(x_train, y_train)
+
+    tree_file = open(file_str + ".model", "wb")
+    pickle.dump(tree, tree_file)
+    tree_file.close()
+
+    print("Tree saved")
+    main()
+
+    #clf_gini.predict([1,1,0,0,0])
+
+    #predict = clf_gini.predict(x_test)
+
+    #print("Accuracy ", accuracy_score(x_test, predict)*100)
+
+
+	    
 	
 def case_2():
+    print("Tree saved")
+    main()
 
 		
 	
@@ -43,39 +64,43 @@ def case_3():
 	if (choice == 2):
 		return
 		
-	else if (choice == 1):
+	elif (choice == 1):
+        print("Enter new data\n")
 		
 		
 	
 def case_4():
 
+    model_name = input("Enter name of model file")
+
 def main():
 
-	print("Choose an option\n")
-	print("1: Learn a decision tree from training data\n")
-	print("2: Save the tree\n")
-	print("3: Apply the tree to new cases\n")
-	print("4: Load a tree model saved previously and apply the model to new cases\n")
-	print("5: Quit\n")
-	
-	#choice = input("Enter choice\n");
-	
-	while(1):
-		choice = input("Enter choice\n");
-		if (choice == 1):
-			case_1()
-		
-		else if (choice == 2):
-			case_2()
-			
-		else if (choice == 3):
-			case_3()
-		
-		else if (choice == 4):
-			case_4()
-		
-		else if (choice == 5):
-			#sys.exit()
-			return
-		
-	
+    print("Choose an option\n")
+    print("1: Learn a decision tree from training data\n")
+    print("2: Save the tree\n")
+    print("3: Apply the tree to new cases\n")
+    print("4: Load a tree model saved previously and apply the model to new cases\n")
+    print("5: Quit\n")
+
+    #choice = input("Enter choice\n");
+
+    choice = input("Enter choice\n")
+
+    if (choice == '1'):
+        case_1()
+
+    elif (choice == '2'):
+        case_2()
+
+    elif (choice == '3'):
+        case_3()
+
+    elif (choice == '4'):
+        case_4()
+
+    elif (choice == '5'):
+        #sys.exit()
+        return
+
+
+main()
