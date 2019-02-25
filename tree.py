0acd4004ab1f05dd243e60e009087300351cdac7
Maria <anr66@zips.uakron.edu>
2019-02-24 21:34:40 -0500

    Complete step 3

--- tree.py
+++ tree.py
@@ -9,11 +9,13 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 
 def case_1():
     print("hello")
 
+    global file_str
     file_str = input("Enter the name of the data file\n")
 
     data = pd.read_csv(file_str, sep=',')
@@ -23,24 +25,37 @@ def case_1():
 
     #print("Data: ", data.head())
 
-    global X
-    X = data.values[:, 0:1]
-    Y = data.values[:, 0]
+    #global X
+    #X = data.values[:, 0:1]
+    #Y = data.values[:, 0]
+
+    X = data.drop('color', axis=1)
+    y = data['color']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+    global classifier
+    classifier = DecisionTreeClassifier()
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+    #print("X_test")
+    #print(X_test)
+
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
     #x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 100)
 
     #clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100, max_depth = 3, min_samples_leaf = 100)
 
-    global classifier
-    classifier = tree.DecisionTreeClassifier()
-    classifier = classifier.fit(X, Y)
+    #global classifier
+    #classifier = tree.DecisionTreeClassifier()
+    #classifier = classifier.fit(X, Y)
     #tree = clf_gini.fit(x_train, y_train)
 
-    tree_file = open(file_str + ".model", "wb")
-    pickle.dump(classifier, tree_file)
-    tree_file.close()
 
-    print("Tree saved")
+
     main()
 
     #clf_gini.predict([1,1,0,0,0])
@@ -50,14 +65,18 @@ def case_1():
     #print("Accuracy ", accuracy_score(x_test, predict)*100)
 
 
-	    
-	
+
+
 def case_2():
+
+    tree_file = open(file_str + ".model", "wb")
+    pickle.dump(classifier, tree_file)
+    tree_file.close()
     print("Tree saved")
     main()
 
-		
-	
+
+
 def case_3():
 
     print("1: Enter new case\n")
@@ -68,14 +87,43 @@ def case_3():
         return
 
     elif (choice == '1'):
-        #new_data = input("Enter new data\n")
-        new_data = ['1','1','0','0','1']
-        label = classifier.predict(X,new_data)
-        print (label[0])
 
-	main()
-		
-	
+        new_data = input("Enter new data\n")
+        #y_pred = classifier.predict(['0','1','1','1','1'])
+
+        with open(file_str, 'a') as csv:
+            csv.write(new_data)
+
+        data = pd.read_csv(file_str, sep=',')
+
+        print("Length: ", len(data))
+        #print("Shape: ", data.shape())
+
+        #print("Data: ", data.head())
+
+        #global X
+        #X = data.values[:, 0:1]
+        #Y = data.values[:, 0]
+
+        X = data.drop('color', axis=1)
+        y = data['color']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+        global classifier
+        classifier = DecisionTreeClassifier()
+        classifier.fit(X_train, y_train)
+
+        y_pred = classifier.predict(X_test)
+        #print("X_test")
+        #print(X_test)
+
+        print(confusion_matrix(y_test, y_pred))
+        print(classification_report(y_test, y_pred))
+
+    main()
+
+
 def case_4():
 
     model_name = input("Enter name of model file\n")
@@ -85,13 +133,15 @@ def case_4():
 
 def main():
 
-    print("\n\n\n")
+    print("\n")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Choose an option\n")
     print("1: Learn a decision tree from training data\n")
     print("2: Save the tree\n")
     print("3: Apply the tree to new cases\n")
     print("4: Load a tree model saved previously and apply the model to new cases\n")
-    print("5: Quit\n")
+    print("5: Quit")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
     #choice = input("Enter choice\n");
 
